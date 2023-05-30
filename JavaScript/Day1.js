bb61e2455ed3248c55290afa9b47ad6416579609
Dilnawaz <mddilnawazalam7739@gmail.com>
2023-05-30 17:28:32 +0530

    javascript

---
+++ JavaScript/Day1.js
@@ -0,0 +1,29 @@
+
+/*
+alert ("hello babes did you missed me");
+
+alert(3+2
+    +1
+    );
+[1,4,5].forEach(alert);
+
+this is used to comment the things
+*/
+
+let message;
+message="hello";
+alert(message);
+
+let user ='john',age=25, saying ="i am john";   // we can also write code in this way with only declaring one variable ;
+
+let name="dilnawaz",
+dage=21,
+msg="do it now";
+
+alert(msg)
+
+
+
+
+
+
